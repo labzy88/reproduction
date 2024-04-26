69ca16b1e6668ee0af685037058f5481b1e30889
Christopher Labrador <chris@eatbyalex.com>
2024-04-26 15:40:40 +0200

    added fk-serialised test case

---
+++ src/fk-serialised.test.ts
@@ -0,0 +1,90 @@
+import { Entity, ManyToOne, MikroORM, PrimaryKey, PrimaryKeyProp, Property, wrap } from '@mikro-orm/sqlite';
+
+@Entity()
+class User {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name: string;
+
+  @Property({ unique: true })
+  email: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+
+}
+
+@Entity()
+class Debt {
+
+  [PrimaryKeyProp]?: ['lender', 'debtor'];
+
+  @ManyToOne({ primary: true, updateRule: 'cascade', deleteRule: 'cascade' })
+  lender!: User;
+
+  @ManyToOne({ primary: true, updateRule: 'cascade', deleteRule: 'cascade' })
+  debtor!: User;
+
+  @Property()
+  amount: number = 0;
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: ':memory:',
+    entities: [User, Debt],
+    debug: ['query', 'query-params'],
+    allowGlobalContext: true, // only for testing
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('basic CRUD example', async () => {
+  orm.em.create(User, { name: 'Foo', email: 'foo' });
+  await orm.em.flush();
+  orm.em.clear();
+
+  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
+  expect(user.name).toBe('Foo');
+  user.name = 'Bar';
+  orm.em.remove(user);
+  await orm.em.flush();
+
+  const count = await orm.em.count(User, { email: 'foo' });
+  expect(count).toBe(0);
+});
+
+test('FK when serialized', async () => {
+  const debt = new Debt();
+  debt.lender = new User('Foo', 'foo');
+  debt.debtor = new User('Bar', 'bar');
+
+  await orm.em.persistAndFlush(debt);
+
+  const lenderPopulated = await orm.em.findOneOrFail(Debt, { lender: debt.lender }, { populate: ['lender'] });
+  expect(lenderPopulated.lender.id).toBeDefined();
+
+  const sLenderPopulated = wrap(lenderPopulated).toObject();
+  expect(sLenderPopulated.lender).toBeDefined();
+  expect(sLenderPopulated.lender.id).toBeDefined();
+
+  const lenderUnpopulated = await orm.em.findOneOrFail(Debt, { lender: debt.lender }, { populate: [] });
+  expect(lenderUnpopulated.lender).toBeDefined(); // should be FK
+  expect(lenderUnpopulated.lender.id).toBeUndefined(); // should not exist ??
+
+  const sLenderUnpopulated = wrap(lenderUnpopulated).toObject();
+  expect(sLenderUnpopulated.lender).toBeDefined(); // should be FK
+  expect(sLenderUnpopulated.lender.id).toBeUndefined(); // should no exist ??
+});
